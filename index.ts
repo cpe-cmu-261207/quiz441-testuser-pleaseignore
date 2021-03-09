c2d268e1b63924b83a28bd6210fde391236c71fd
testuser-pleaseignore <test_user_please_ignore@hotmail.com>
2021-03-09 15:06:11 +0700

    commit

--- index.ts
+++ index.ts
@@ -4,17 +4,24 @@ import cors from 'cors'
 import bcrypt from 'bcrypt'
 import jwt from 'jsonwebtoken'
 import { body, query, validationResult } from 'express-validator'
+import fs from 'fs'
+
 
 const app = express()
-app.use(bodyParser.json())
-app.use(cors())
-
 const PORT = process.env.PORT || 3000
 const SECRET = "SIMPLE_SECRET"
+app.use(bodyParser.json())
+app.use(cors())
 
 interface JWTPayload {
   username: string;
   password: string;
+}
+
+const readDbFile = () => {
+  const raw = fs.readFileSync('db.json', 'utf8')
+  const db = JSON.parse(raw)
+  return db
 }
 
 app.post('/login',
@@ -26,20 +33,57 @@ app.post('/login',
     return res.status(200).json({
       message: 'Login succesfully',
     })
+    const { username, password } = req.body
+    const db = readDbFile()
+    const user = db.users.find((data : any) => data.username === username)
+    if (!user) {
+      res.status(400)
+      res.json({ message: "Invalid username or password" })
+      return
+    }
+    if (!bcrypt.compareSync(password, user.password)) {
+      res.status(400)
+      res.json({ message: "Invalid username or password" })
+      return
+    }
+    const token = jwt.sign({username: user.username,password: user.password } as JWTPayload , SECRET)
+    return res.status(200).json({ message:"Login successfully", token})
   })
 
 app.post('/register',
   (req, res) => {
 
     const { username, password, firstname, lastname, balance } = req.body
-  })
+    const errors = validationResult(req)
+      const db = readDbFile()
+      if (db.users.find((data: any) => data.username == username)) {
+        res.status(400).json({message: "This username is already in used."})
+        return
+      }
+      const passwordhasher = bcrypt.hashSync(password, 16)
+      db.users.push({
+        id: Date.now(),
+        username,
+        password: passwordhasher,
+        balance 
+      })
+      fs.writeFileSync('db.json', JSON.stringify(db))
+      res.status(200).json({ message: "Register complete" })
+      res.status(400).json({ message: "Invalid username or password" })
+    })
 
 app.get('/balance',
   (req, res) => {
     const token = req.query.token as string
     try {
       const { username } = jwt.verify(token, SECRET) as JWTPayload
-  
+      const db = readDbFile()
+      const user = db.users.find((data : any) => data.username === username)
+      if(user){
+        res.status(200).json({user.username,user.balance})
+      }else{
+        res.status(400).json({user.message})
+      }
     }
     catch (e) {
       //response in case of invalid token
@@ -62,7 +106,7 @@ app.post('/withdraw',
 app.delete('/reset', (req, res) => {
 
   //code your database reset here
-  
+  fs.writeFileSync('db.json', JSON.stringify({users:[]}))
   return res.status(200).json({
     message: 'Reset database successfully'
   })
